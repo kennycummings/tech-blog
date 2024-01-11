05dc117df5cee3c423558fe209bfbe528d30ea8e
kennycummings <kennethccummings@gmail.com>
2024-01-11 13:10:34 -0500

    Added controller .js files

---
+++ Develop/controllers/api/dashboardRoutes.js
@@ -0,0 +1,6 @@
+const router = require('express').Router();
+const { Post } = require('../../models');
+
+// Add routes for retrieving, updating, and deleting posts
+
+module.exports = router;
---
+++ Develop/controllers/api/homeRoutes.js
@@ -0,0 +1,6 @@
+const router = require('express').Router();
+const { Post, User, Comment } = require('../models');
+
+// Add routes for homepage and viewing individual posts
+
+module.exports = router;
