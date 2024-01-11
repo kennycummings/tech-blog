d2767de3e6f87f90bbf5f3f905702bd9dc9399e0
kennycummings <kennethccummings@gmail.com>
2024-01-11 13:02:17 -0500

    Added api and model files

---
+++ Develop/controllers/api/comment_Routes.js
@@ -0,0 +1,6 @@
+const router = require('express').Router();
+const { Comment } = require('../../models');
+
+// Add routes for creating, updating, and deleting comments
+
+module.exports = router;
---
+++ Develop/controllers/api/postRoutes.js
@@ -0,0 +1,6 @@
+const router = require('express').Router();
+const { Post, Comment } = require('../../models');
+
+// Add routes for creating, updating, and deleting posts
+
+module.exports = router;
