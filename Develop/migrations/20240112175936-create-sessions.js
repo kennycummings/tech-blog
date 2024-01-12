400d758fdfaec1d65b21d952e02baefd5c593046
kennycummings <kennethccummings@gmail.com>
2024-01-12 13:01:44 -0500

    Got the front end to work; generated migrations folder

---
+++ Develop/migrations/20240112175936-create-sessions.js
@@ -0,0 +1,30 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Sessions', {
+      sid: {
+        type: Sequelize.STRING,
+        primaryKey: true,
+      },
+      expires: {
+        type: Sequelize.DATE,
+      },
+      data: {
+        type: Sequelize.TEXT,
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
+  },
+
+  down: async (queryInterface) => {
+    await queryInterface.dropTable('Sessions');
+  },
+};
